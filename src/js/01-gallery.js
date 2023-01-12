f6e527271b1cae58fc8d1d93ed58d0409b0a61c3
Ira Akulova <ira.akulova2019@gmail.com>
2023-01-12 10:27:11 +0200

    01 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGallaryMurkup(galleryItems);
+
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallaryMurkup(img) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        data-title="${description}"
+        />
+        </a>
+        </div>`;
+    }).join('');
+
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
