4fd1d48baaebad747e9e5424e6c0a3a4d1f1d471
Ira Akulova <ira.akulova2019@gmail.com>
2023-01-17 19:09:38 +0200

    03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    emailEl: document.querySelector('input'),
+    messageEl: document.querySelector('textarea'),
+};
+const formData = {};
+const STORAGE_KEY = "feedback-form-state";
+
+refs.formEl.addEventListener("submit", onFormSubmit);
+refs.formEl.addEventListener("input", throttle((onFormInput), 1000));
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+};
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+function populateForm() {
+    const savedMsg = localStorage.getItem(STORAGE_KEY);
+    const parcedMsg = JSON.parse(savedMsg);
+
+    if (savedMsg) {
+        refs.emailEl.value = parcedMsg.email;
+        refs.messageEl.value = parcedMsg.message;
+    }
+    
+};
+populateForm();
