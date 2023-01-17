d7e6b3a85ba6d4a8d0450c5743826e75744b9612
Ira Akulova <ira.akulova2019@gmail.com>
2023-01-17 11:40:36 +0200

    01 and 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+
+const iframe = document.querySelector('iframe');
+    const iframePlayer = new Player(iframe);
+
+    iframePlayer.on('play', function() {
+        console.log('played the video!');
+    });
+
+    iframePlayer.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+iframePlayer.on('timeupdate', throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
